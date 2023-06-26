334cc162b97b296f1ed100ca91af42d2c9e28209
Lee816 <lgha0816@naver.com>
2023-06-26 14:35:02 +0900

    post_all, post_detail 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import generic
+
+from .models import Post
 
 # Create your views here.
+
+
+class PostLV(generic.ListView):
+    model = Post
+    template_name = "blog/post_all.html"
+    context_object_name = "posts"
+    paginate_by = 2
+
+
+class PostDV(generic.DetailView):
+    model = Post
