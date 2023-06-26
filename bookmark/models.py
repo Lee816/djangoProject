1df818f61a94c4861b87ce46f9db66ebee5397ac
Lee816 <lgha0816@naver.com>
2023-06-26 11:02:32 +0900

    bookmark 모델 생성

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bookmark(models.Model):
+    title = models.CharField("TITLE", max_length=100, blank=True)  # 'TITLE' 데이터베이스 이름
+    url = models.URLField("URL", unique=True)
+
+    def __str__(self) -> str:
+        return self.title
