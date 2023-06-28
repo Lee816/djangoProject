95b802736a656dcc8306ffb5e47ee0c6be4143a4
Lee816 <lgha0816@naver.com>
2023-06-28 14:38:36 +0900

    urls 에 간단한 view를 만드는 방법

--- photo/urls.py
+++ photo/urls.py
@@ -8,3 +8,12 @@ urlpatterns = [
     path("album/<int:pk>/", views.AlbumDV.as_view(), name="album_detail"),
     path("photo/<int:pl>/", views.PhotoDV.as_veiw(), name="photo_detail"),
 ]
+
+# from django.views.generic import ListView, DetailView
+
+# urlpatterns = [
+#     path("", ListView.as_view(model = Album), name="index"),
+#     path("album", ListView.as_view(model = Album), name="albim_list"),
+#     path("album/<int:pk>/", DetailView.as_view(model = Album), name="album_detail"),
+#     path("photo/<int:pl>/", DetailView.as_veiw(model = Photo), name="photo_detail"),
+# ]
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import generic
+
+from .models import Photo, Album
+
 
 # Create your views here.
+class AlbumLV(generic.ListView):
+    model = Album
+
+
+class AlbumDV(generic.DetailView):
+    model = Album
+
+
+class PhotoDV(generic.DetailView):
+    model = Photo
