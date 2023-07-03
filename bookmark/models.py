be12a81e2bc204bfd75a5e08e8cb0308342043ba
Lee816 <lgha0816@naver.com>
2023-07-03 10:42:50 +0900

    유저 권한 모델 수정

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 
@@ -7,6 +9,7 @@ class Bookmark(models.Model):
     # '이름' -> admin 사이트에서 나타나는 별칭(verbose_name)
     title = models.CharField("TITLE", max_length=100, blank=True)
     url = models.URLField("URL", unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title
