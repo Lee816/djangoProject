41e67e4150f8f721b4410bea319aef721b660019
Lee816 <lgha0816@naver.com>
2023-07-06 10:11:30 +0900

    photo 앱 photo CRUD 작성

--- photo/views.py
+++ photo/views.py
@@ -1,4 +1,8 @@
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+
+from mysite.views import OwnerOnlyMixin
 
 from .models import Photo, Album
 
@@ -14,3 +18,27 @@ class AlbumDV(generic.DetailView):
 
 class PhotoDV(generic.DetailView):
     model = Photo
+
+
+class PhotoCV(LoginRequiredMixin, generic.CreateView):
+    model = Photo
+    fields = ("album", "title", "image", "description")
+    success_url = reverse_lazy("photo:index")
+
+
+class PhotoChangeLV(LoginRequiredMixin, generic.ListView):
+    model = Photo
+    template_name = "photo/photo_change_list.html"
+
+    def get_queryset(self):
+        return Photo.objects.filter(owner=self.request.user)
+
+
+class PhotoUV(OwnerOnlyMixin, generic.UpdateView):
+    model = Photo
+    success_url = reverse_lazy("photo:index")
+
+
+class PHotoDelV(OwnerOnlyMixin, generic.DeleteView):
+    model = Photo
+    success_url = reverse_lazy("photo:index")
